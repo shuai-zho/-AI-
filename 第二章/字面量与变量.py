65ecad347a7aea29debf914901868187e8bde81c
shuai-zho <2452977562@qq.com>
2026-06-29 12:38:16 +0800

    提交所有文件

---
+++ 第二章/字面量与变量.py
@@ -0,0 +1,17 @@
+# 字面量的写法
+print(100)              # 整数 (int)
+print(3.14)             # 浮点数 (float)
+print(True)             # 布尔值 (bool)
+print(False)            # 布尔值 (bool)
+print("Hello Python")   # 字符串 (str)
+print(None)             # 空值 (NoneType)
+
+
+# 布尔类型本质也是整数类型(True - 1 ; False - 0)
+print(True + 1)         # 输出 2
+print(False - 1)        # 输出 -1
+
+
+# 变量
+num = 1144.1
+
